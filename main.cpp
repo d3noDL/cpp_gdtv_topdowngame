8f7a7625834a2e547998f8cc9de41e0668daa6bd
d3noDL <dino@adenogame.com>
2023-09-15 23:02:14 +0200

    Movement

--- main.cpp
+++ main.cpp
@@ -7,14 +7,26 @@ int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Top Down Game");
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 map_position {0.0, 0.0};
+    float speed {4.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
         /* BEGIN LOGIC */
+
+        Vector2 direction {};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0) {
+            
+            map_position = Vector2Subtract(map_position, Vector2Scale(Vector2Normalize(direction), speed));
+        }
         
-        Vector2 map_position {0.0, 0.0};
+        
         DrawTextureEx(map, map_position , 0, 4, WHITE);
         
         /* END LOGIC */
