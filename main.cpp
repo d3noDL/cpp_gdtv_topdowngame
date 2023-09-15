3a0120e9d1d54e2d45acb36aae45208d1f9160a6
d3noDL <dino@adenogame.com>
2023-09-15 22:45:05 +0200

    Game setup

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include "raylib.h"
 
 int main() {
-    
+    const int WINDOW_WIDTH {384};
+    const int WINDOW_HEIGHT {384};
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Top Down Game");
+
+    Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        /* BEGIN LOGIC */
+        
+        Vector2 map_position {0.0, 0.0};
+        DrawTextureEx(map, map_position , 0, 4, WHITE);
+        
+        /* END LOGIC */
+        EndDrawing();
+    }
+    UnloadTexture(map);
+    CloseWindow()
 }
