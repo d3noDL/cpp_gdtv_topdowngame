20285d7b59903f27057ac49fecd5240158aaa1ea
d3noDL <dino@adenogame.com>
2023-09-16 22:39:40 +0200

    Enemy screen position

--- enemy.cpp
+++ enemy.cpp
@@ -1,4 +1,5 @@
 #include "enemy.h"
+#include "raymath.h"
 
 Enemy::Enemy(Vector2 pos, Texture2D idle_tex, Texture2D run_tex) {
         world_position = pos;
@@ -10,5 +11,7 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_tex, Texture2D run_tex) {
 }
 
 void Enemy::tick(float deltaTime){
+    screen_position = Vector2Subtract(world_position, target->getWorldPosition());
+
     BaseCharacter::tick(deltaTime);
 }
