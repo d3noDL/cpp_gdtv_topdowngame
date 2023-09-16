a99431021618698676d4f106c966039e15806ee5
d3noDL <dino@adenogame.com>
2023-09-16 22:54:19 +0200

    Enemy AI

--- character.h
+++ character.h
@@ -7,6 +7,7 @@
 class Character : public BaseCharacter {
     public:
         Character(int winWidth, int winHeight);
+        Vector2 getScreenPosition() { return screen_position;}
         virtual void tick(float deltaTime) override; // Overriding BaseCharacter tick function
     private:
         
--- enemy.cpp
+++ enemy.cpp
@@ -1,16 +1,26 @@
 #include "enemy.h"
 #include "raymath.h"
 
+
 Enemy::Enemy(Vector2 pos, Texture2D idle_tex, Texture2D run_tex) {
         world_position = pos;
         texture = idle_tex;
         run = run_tex;
-        
         width = texture.width / max_frames;
         height = texture.height;
+        speed = 3.5f;
 }
 
 void Enemy::tick(float deltaTime){
+    // Get toTarget vector
+    Vector2 toTarget = Vector2Subtract(target->getScreenPosition(), screen_position);
+    // Normalize toTarget
+    toTarget = Vector2Normalize(toTarget);
+    // Multiply toTarget by speed
+    toTarget = Vector2Scale(toTarget, speed);
+    // Move the enemy towards the character
+    world_position = Vector2Add(world_position, toTarget);
+    
     screen_position = Vector2Subtract(world_position, target->getWorldPosition());
 
     BaseCharacter::tick(deltaTime);
